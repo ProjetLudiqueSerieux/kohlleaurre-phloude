a6aa6043a5bd0a0599220b162db1c54b0de63193
QuentinDurand <quentin2.durand@wanadoo.fr>
2017-04-25 15:02:26 +0200

    Update boucle_jeu.c

--- boucle_jeu.c
+++ boucle_jeu.c
@@ -3,4 +3,25 @@
 #include "grille.h"
 #include "tache.h"
 
+int main()
+{
+  printf("Entrez la taille du plateau de jeu : \n");
+  int n = 0;
+  while (n <= 0)
+  {
+    scanf("%i",&n);
+    if (n <= 0)
+      printf("Veuillez saisir un entier positif non nul");
+  }
+  printf("Entrez le nombre de coup autorisé : \n");
+  int nb_coup = 0;
+  while (n_coup <= 0)
+  {
+    scanf("%i",&nb_coup);
+    if (nb_coup <= 0)
+      printf("Veuillez saisir un entier positif non nul");
+  }
+  
+  return 0;
+}
 
