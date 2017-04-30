bbcdcf8499eb7ac4923abcd41d324b1a7aed3e9c
QuentinDurand <quentin2.durand@wanadoo.fr>
2017-04-30 13:56:37 +0200

    Update lotB_affichage.c

--- lotB_affichage.c
+++ lotB_affichage.c
@@ -20,8 +20,40 @@ void affiche_grille(int ** grille,int n,int r)
   printf("coups restants : %i",r);
 }
 
-int main()
+/**
+*\fn void purger(void)
+*\brief vide le flux pour permetrte a scanf de fonctionner
+*\param void
+*\return void
+*/
+
+void purger()
 {
-  return 0;
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
+    }
+}
+
+/**
+*\fn int demande_int(void)
+*\brief attend que l'utilisateur entre un entier et lui fait recommencer s'il a fait une erreur.
+*\param void
+*\return un int entré par l'utilisateur
+*/
+
+int demande_int()
+{
+  int res = 0;
+  int verif = 'n';
+  while(verif == 'n')
+    {
+      scanf("%i",&res);
+      purger();
+      printf("Vous avez entré %i. n pour recommencer, o sinon.\n",res);
+      verif = getchar();
+      purger();
+    }
+  return res;
 }
 
