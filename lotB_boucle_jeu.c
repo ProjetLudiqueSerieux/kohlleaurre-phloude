245f51b681ec116f829ae2a5154e8c527f8f9bc3
QuentinDurand <quentin2.durand@wanadoo.fr>
2017-04-30 14:23:45 +0200

    Add files via upload

---
+++ lotB_boucle_jeu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grille.h"
+#include "tache.h"
+#include "affichage.h"
+
+int main()
+{
+  srand(time(NULL);
+  printf("Entrez la taille du plateau de jeu : \n");
+  int n = demande_int(); 
+  printf("Entrez le nombre de coup autorisé : \n");
+  int nb_coup = demande_int();
+  return 0;
+}
