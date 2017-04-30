47fd467dbaafe20792a4a86d2e7a5d0735b3e115
QuentinDurand <quentin2.durand@wanadoo.fr>
2017-04-30 13:15:13 +0200

    Add files via upload

---
+++ lotB_affichage.c
@@ -0,0 +1,27 @@
+#include "grille.h"
+#include "tache.h"
+#include "lotB_debut.h"
+#include "lotB_affichage.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char couleur[6] = {'R','J','B','V','O','C'};
+
+
+void affiche_grille(int ** grille,int n,int r)
+{
+  int i,j;
+  for (i=0;i<n;i++)
+    {
+    for(j=0;j<n;j++)
+      printf("%c  ",couleur[grille[i][j]]);
+    printf("\n");
+    }
+  printf("coups restants : %i",r);
+}
+
+int main()
+{
+  return 0;
+}
+
